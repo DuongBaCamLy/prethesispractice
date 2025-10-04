20e4a9bbaf765ea37765da5e6fef88c077c3efce
ly <lduong974@gmail.com>
2025-10-05 00:41:12 +0700

    step 7

--- 00-react-javascript/src/pages/login.jsx
+++ 00-react-javascript/src/pages/login.jsx
@@ -1,6 +1,69 @@
+
+
+import { Button, Form, Input, notification } from 'antd';
+import { loginApi } from '../util/api';
+import { useNavigate } from 'react-router-dom';
+
+
 const LoginPage = () => {
-    return (
-        <>Login Page</>
-    );
-}
+  const navigate= useNavigate();
+    const onFinish = async (values) => {
+      const {email, password}= values;
+
+
+      const res= await loginApi(email, password);
+      debugger
+      if(res && res.EC===0){
+        localStorage.setItem("access_token", res.access_token)
+        notification.success({
+          message:"LOGIN USER",
+          description:"Success"
+        });
+        navigate("/");
+      }else{
+        notification.error({
+          message:"LOGIN FAIL",
+          description: res ?.EM ??"Error"
+        })
+      }
+    };
+ return (
+    <div style={{ margin: '50px' }}>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+
+
+        onFinish={onFinish}
+        autoComplete="off"
+        layout='vertical'
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: 'Please input your Email!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
 export default LoginPage;
--- 00-react-javascript/vite.config.js
+++ 00-react-javascript/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/v1': {
-        target: 'http://localhost:8082', // hoặc 8081 nếu backend của bạn ở đó
+        target: 'http://localhost:8085', // hoặc 8081 nếu backend của bạn ở đó
         changeOrigin: true,
       },
     },
